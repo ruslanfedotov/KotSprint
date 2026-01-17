d24554235f68b87c3ede450f0269c60dd8bbbbf1
Ruslan <fedotovoruslan617@gmail.com>
2026-01-17 19:49:33 +0300

    KS 1-2 Вывод переменных

---
+++ src/lesson1/task3.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+}
