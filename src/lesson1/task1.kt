02fa9f74e46c2c55ba2664ae39da79c8f3fc7f89
Ruslan <fedotovoruslan617@gmail.com>
2026-01-17 19:24:40 +0300

    KS 1-1 Исправление, принудительно задал тип данных у строки

--- src/lesson1/task1.kt
+++ src/lesson1/task1.kt
@@ -2,5 +2,5 @@ package lesson1
 
 fun main(){
     var NumberOfOrders: Int = 75
-    var TextOfThankfullness = "Благодарим за покупку!"
+    var TextOfThankfullness: Stirng = "Благодарим за покупку!"
 }
