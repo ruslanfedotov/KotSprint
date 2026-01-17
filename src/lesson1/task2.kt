1ec5011c3fe05c2269e4595c785e9f14bb131b18
Ruslan <fedotovoruslan617@gmail.com>
2026-01-17 19:38:39 +0300

    KS 1-2 Вывод переменных

---
+++ src/lesson1/task2.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main(){
+    var NumberOfOrders: Int = 75
+    var TextOfThankfullness: String = "Благодарим за покупку!"
+    var NumberOfEmployees: Int = 2000
+
+    println(NumberOfOrders)
+    println(TextOfThankfullness)
+//    println(NumberOfEmployees)
+
+    NumberOfEmployees = 1999
+    println(NumberOfEmployees)
+}
