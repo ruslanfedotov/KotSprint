221144ad02c7e29c1f3ff1fe9e905e1afeb03a63
Ruslan <fedotovoruslan617@gmail.com>
2026-01-18 16:56:32 +0300

    KS 1-1 Исправления

--- src/lesson1/task1.kt
+++ src/lesson1/task1.kt
@@ -1,6 +1,6 @@
 package lesson1
 
-fun main(){
-    var NumberOfOrders: Int = 75
-    var TextOfThankfullness: String = "Благодарим за покупку!"
+fun main() {
+    val numberOfOrders: Int = 75
+    val textOfThankfullness: String = "Благодарим за покупку!"
 }
