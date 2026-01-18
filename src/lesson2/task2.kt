3df9d279aa73156139fa7519f6673d35f84a943f
Ruslan <fedotovoruslan617@gmail.com>
2026-01-18 17:35:50 +0300

    KS-2-2 Зарплаты

---
+++ src/lesson2/task2.kt
@@ -0,0 +1,12 @@
+package lesson2
+
+fun main() {
+    val salaryOfEmployees: Int = 50 * 30000
+    val generalExpenses: Int = salaryOfEmployees + 30 * 20000
+    val averageSalary: Int = generalExpenses / (50 + 30)
+
+    println(salaryOfEmployees)
+    println(generalExpenses)
+    println(averageSalary)
+
+}
