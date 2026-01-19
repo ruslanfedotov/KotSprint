249fc5a17a7a97e88851b5e644b1ca16773c5d62
Ruslan <fedotovoruslan617@gmail.com>
2026-01-19 18:56:38 +0300

    KS-3-1 Приветственное сообщение

---
+++ src/lesson3/task1.kt
@@ -0,0 +1,12 @@
+package lesson3
+
+fun main() {
+    val userName = "Руслан"
+    var welcomeMessage = "Добрый день, $userName"
+
+    println(welcomeMessage)
+
+    welcomeMessage = "Добрый вечер, $userName"
+
+    println(welcomeMessage)
+}
