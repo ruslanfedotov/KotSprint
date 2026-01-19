a2b9574833bc73aef4313a49b1176771cc07f13d
Ruslan <fedotovoruslan617@gmail.com>
2026-01-19 21:14:10 +0300

    KS-4-1 Сделал в одном println

--- src/lesson4/task1.kt
+++ src/lesson4/task1.kt
@@ -4,9 +4,7 @@ fun main() {
     val tablesToday: Int = 13
     val tablesTomorrow: Int = 9
 
-    println("Доступность столиков на сегодня: ${tablesToday < NUMBER_OF_TABLES}")
-    println("Доступность столиков на завтра: ${tablesTomorrow < NUMBER_OF_TABLES}")
-
+    println("Доступность столиков на сегодня: ${tablesToday < NUMBER_OF_TABLES}\nДоступность столиков на завтра: ${tablesTomorrow < NUMBER_OF_TABLES}")
 }
 
 const val NUMBER_OF_TABLES = 13
