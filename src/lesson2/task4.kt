213ccdd2819ef3b3f0172a7b7228b822d02a6606
Ruslan <fedotovoruslan617@gmail.com>
2026-01-23 21:08:49 +0300

    KS-2-4 Исправление

--- src/lesson2/task4.kt
+++ src/lesson2/task4.kt
@@ -3,9 +3,11 @@ package lesson2
 fun main() {
     val crystal: Int = 7
     val iron: Int = 11
-    val buffCrystal: Int = (crystal * 0.2).toInt()
-    val buffIron: Int = (iron * 0.2).toInt()
+    val buffCrystal: Int = (crystal * BUFF / 100)
+    val buffIron: Int = (iron * BUFF / 100)
 
     println("Бонусных кристаллов: $buffCrystal")
     println("Бонусного железа: $buffIron")
 }
+
+const val BUFF = 20
