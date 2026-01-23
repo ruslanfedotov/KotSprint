fb43863054e8e73a4ef5ce59f2f2cf1efe009131
Ruslan <fedotovoruslan617@gmail.com>
2026-01-23 21:01:19 +0300

    KS-2-2 Исправление

--- src/lesson2/task2.kt
+++ src/lesson2/task2.kt
@@ -1,9 +1,13 @@
 package lesson2
 
 fun main() {
-    val salaryOfEmployees: Int = 50 * 30000
-    val generalExpenses: Int = salaryOfEmployees + 30 * 20000
-    val averageSalary: Int = generalExpenses / (50 + 30)
+    val numberOfEmployees: Int = 50
+    val salaryOfEmployees: Int = 30000
+    val numberOfInterns: Int = 30
+    val salaryOfInterns: Int = 20000
+    val salaryOfAllEmployees: Int = numberOfEmployees * salaryOfEmployees
+    val generalExpenses: Int = salaryOfAllEmployees + numberOfInterns * salaryOfInterns
+    val averageSalary: Int = generalExpenses / (numberOfEmployees + numberOfInterns)
 
     println(salaryOfEmployees)
     println(generalExpenses)
